cccd9f78cb9cb657b6a7ce64b02d61c528a69dea
Florea Alina <floreaalina340@gmail.com>
2026-03-28 12:59:23 +0200

    Add Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,15 @@
+public class Ex1 {
+    public static void main(String[] args) {
+
+        int a = -6+8*6;
+        int b = (44+8)%5;
+        int c = 19 + 3*2/8;
+        int d = 5 + 20/3*2 - 8%4;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+    }
+}
