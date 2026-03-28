2f152b8e4bb42976813c4e08d27f0367b877de67
Florea Alina <floreaalina340@gmail.com>
2026-03-28 14:26:31 +0200

    Add Ex14

---
+++ src/Ex14.java
@@ -0,0 +1,10 @@
+public class Ex14 {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 20; i++) {
+            if (i == 14) continue;
+
+            System.out.println(i);
+        }
+    }
+}
