ce6e90ffe3989140e94242d984e15ecbd3e34cfd
Florea Alina <floreaalina340@gmail.com>
2026-03-28 14:25:16 +0200

    Add Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,11 @@
+public class Ex11 {
+
+    public static void main(String[] args) {
+
+        String text = "Buna";
+
+        System.out.println("Indicele maxim este: " + (text.length()-1));
+        System.out.println("Cantitatea este: " + text.length());
+
+    }
+}
