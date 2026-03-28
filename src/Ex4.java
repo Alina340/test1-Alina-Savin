2a30cb61d679e5f1eca93a349e97dd12bc6434be
Florea Alina <floreaalina340@gmail.com>
2026-03-28 13:23:38 +0200

    Add Ex4

---
+++ src/Ex4.java
@@ -0,0 +1,18 @@
+public class Ex4 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 55;
+
+        if(a > b) {
+            System.out.println("Numarul mai mare este " + a);
+            System.out.println("Numarul mai mic este " + b);
+        }
+        else {
+            System.out.println("Numarul mai mare este " + b);
+            System.out.println("Numarul mai mic este " + a);
+        }
+
+
+    }
+}
