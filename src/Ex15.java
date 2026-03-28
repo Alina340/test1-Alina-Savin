d568ec9c0d766ed563a163d06c844683a10f4ac6
Florea Alina <floreaalina340@gmail.com>
2026-03-28 14:26:48 +0200

    Add Ex15

---
+++ src/Ex15.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Ex15 {
+
+    public static void main(String[] args) {
+
+        byte [] nr = {1, 2, 3 ,4, 5, 6};
+        System.out.println(Arrays.toString(nr));
+
+        nr[4] = 10;
+        System.out.println(Arrays.toString(nr));
+
+
+
+    }
+}
