632ff3973db5d64a24adc71cebbc1edb6a46f5f9
Florea Alina <floreaalina340@gmail.com>
2026-03-28 13:33:07 +0200

    Add Ex5

---
+++ src/Ex5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input password: ");
+        String password = scan.nextLine();
+
+        System.out.println("Password was: " + password);
+    }
+}
